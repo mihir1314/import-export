7124237796ead1b0773cf7bb9d692b1c74f4df61
mihirjariwala1113 <81627330+miku1113@users.noreply.github.com>
2024-10-19 10:09:08 +0530

    Update connect.php

--- auth/connect.php
+++ auth/connect.php
@@ -1,12 +1,13 @@
 <?php
 // Database connection
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "mysql-4ddad7-mihirjariwala334-4557.g.aivencloud.com";
+$username = "avnadmin";
+$password = "AVNS_Xmec4zkLISdSk5ZNMiA";
 $dbname = "import-export";
+$port = 18193; // Specify the port number
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Check connection
 if ($conn->connect_error) {
